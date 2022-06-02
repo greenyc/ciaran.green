0b2ddc6b8e084fab3b1f6fac99d1f09ad58b265e
Ciaran Green <ciarantgreen@gmail.com>
2022-06-02 16:02:15 +0100

    Global styles, moved nav into a component, added small screen warning text

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <SmallScreenWarning />
+    <Navigation />
+    <router-view />
   </div>
-  <router-view />
 </template>
 
-<style>
+<script setup lang="ts">
+import Navigation from "./components/Navigation.vue";
+import SmallScreenWarning from "./components/SmallScreenWarning.vue";
+</script>
+
+<style lang="scss">
+@import "./assets/styles/sheets/_normalize.scss";
+
+.wrapper {
+  width: 100%;
+  max-width: $desktop-width;
+  margin: 0 auto;
+  padding: $quad-unit $double-unit;
+}
+
 #app {
+  width: 100%;
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  background-color: $neutral-dark;
+  color: $neutral-light;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navigation">
+    <router-link to="/" class="navigation__link">Home</router-link> |
+    <router-link to="/about" class="navigation__link">About</router-link>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.navigation {
+  &__link {
+    color: $neutral-light;
+
+    &:visited {
+      color: $neutral-light;
+    }
+  }
+}
+</style>
