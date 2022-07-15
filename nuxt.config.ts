0c6adf9a307b55d6d8ed0e8e25a864262468e7d9
Ciaran Green <ciarantgreen@gmail.com>
2022-07-15 15:28:38 +0100

    Added favicon back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      ],
+    },
+  },
   vite: {
     css: {
       preprocessorOptions: {
