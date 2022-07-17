caca051712204f97ba988b9e23f0833c09127768
Ciaran Green <ciarantgreen@gmail.com>
2022-07-17 20:47:46 +0100

    App CSS in app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,44 @@
 <template>
-  <div>
+  <div class="app">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<style lang="scss">
+@import "@/assets/styles/sheets/_normalize.scss";
+@import "@/assets/styles/mixins/_wrapper.scss";
+
+.app {
+  width: 100%;
+  font-family: "Roboto", sans-serif;
+  color: $neutral-dark;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#__nuxt {
+  width: 100%;
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+}
+
+html {
+  font-size: 10px;
+}
+
+body {
+  margin: 0;
+  font-size: 1.8rem;
+  line-height: 3rem;
+  font-weight: 300;
+}
+
+h1 {
+  font-size: 4rem;
+  line-height: 4.8rem;
+  font-weight: 300;
+}
+</style>
