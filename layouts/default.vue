b6aa10b2179b39a329e20610b76595038c46324a
Ciaran Green <ciarantgreen@gmail.com>
2022-07-17 20:47:18 +0100

    Removed components which I don't want anymore. Removed app CSS from the layout, will move to app.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,7 @@
 <template>
-  <div class="wrapper">
-    <SmallScreenWarning />
-    <Navigation />
+  <div>
     <slot />
   </div>
 </template>
 
-<style lang="scss">
-@import "@/assets/styles/sheets/_normalize.scss";
-
-.wrapper {
-  width: 100%;
-  max-width: $desktop-width;
-  margin: 0 auto;
-  padding: $quad-unit $double-unit;
-}
-
-#__nuxt {
-  width: 100%;
-  min-width: 100vw;
-  min-height: 100vh;
-  display: flex;
-  background-color: $neutral-dark;
-  color: $neutral-light;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-body {
-  margin: 0;
-}
-</style>
+<style lang="scss"></style>
