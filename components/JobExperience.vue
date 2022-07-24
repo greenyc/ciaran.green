632332d26e2063c68e9abddd92745fc3e20590ba
Ciaran Green <ciarantgreen@gmail.com>
2022-07-24 15:18:36 +0100

    Put the contents of each job description under a v-if controlled by clicking a chevron

--- components/JobExperience.vue
+++ components/JobExperience.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="job-experience">
-    <h4 class="job-experience__heading">
-      {{ headingText }}
-    </h4>
-    <p class="job-experience__subheading">{{ subheadingText }}</p>
-    <p
-      v-for="(item, index) in bodyText"
-      :key="index"
-      class="job-experience__info"
-    >
-      {{ item }}
-    </p>
+    <div class="job-experience__row">
+      <div class="job-experience__top">
+        <h4 class="job-experience__heading">
+          {{ headingText }}
+        </h4>
+        <p class="job-experience__subheading">{{ subheadingText }}</p>
+      </div>
+      <SvgChevron
+        :class="[
+          'job-experience__chevron',
+          { 'job-experience__chevron--expanded': expanded },
+        ]"
+        @click="toggleExpanded"
+      />
+    </div>
+    <div v-if="expanded">
+      <p
+        v-for="(item, index) in bodyText"
+        :key="index"
+        class="job-experience__info"
+      >
+        {{ item }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -30,12 +43,49 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+  methods: {
+    toggleExpanded() {
+      this.expanded = !this.expanded;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .job-experience {
   margin-bottom: $triple-unit;
+
+  &__chevron {
+    min-width: 24px;
+    min-height: 24px;
+    width: 24px;
+    height: 24px;
+    margin-left: $double-unit;
+    cursor: pointer;
+
+    :deep(path) {
+      stroke: $neutral-dark-text-light;
+    }
+
+    &--expanded {
+      transform: rotate(180deg);
+    }
+  }
+
+  &__row {
+    display: flex;
+    align-items: center;
+    margin: 0 0 $unit;
+  }
+
+  &__top {
+    flex-grow: 1;
+  }
 
   &__heading {
     margin: 0 0 $half-unit;
@@ -44,8 +94,6 @@ export default {
   &__subheading {
     font-style: italic;
     color: $neutral-dark-text-light;
-
-    margin: 0 0 $unit;
   }
 
   &__info {
